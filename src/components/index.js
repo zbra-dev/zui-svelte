0d79b6c422d0e974e1f400b677341718cb6f8fd4
Uirá Veríssimo <uirado@gmail.com>
2019-08-09 10:01:00 -0300

    Implement Button

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import ToggleButton from "./toggle-button/ToggleButton.svelte";
+import Rating from "./rating/Rating.svelte";
+import Button from "./button/button.svelte";
+
+export { 
+  ToggleButton,
+  Rating,
+  Button,
+}
